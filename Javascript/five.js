cd2f7a1dbb6263e8beced77ab2228cc5456f0f78
gabrielekvietinskaite <gabriele.kvietinskaite@gmail.com>
2020-07-30 00:14:54 +0300

    Add fifth task

---
+++ Javascript/five.js
@@ -0,0 +1,24 @@
+var houses = [
+    {name: "Targaryen", motto: "Fire and Blood"},
+    {name: "Stark", motto: "Winter is Coming"},
+    {name: "Bolton", motto: "Our Blades Are Sharp"},
+    {name: "Greyjoy", motto: "We Do Not Sow"},
+    {name: "Tully", motto: "Family, Duty, Honor"},
+    {name: "Arryn", motto: "As High as Honor"},
+    {name: "Lannister", motto: "Hear Me Roar!"},
+    {name: "Tyrell", motto: "Growing Strong"},
+    {name: "Baratheon", motto: "Ours is the Fury"},
+    {name: "Martell", motto: "Unbowed, Unbent, Unbroken"}
+];
+
+function mottoA(name){
+    for(var i = 0; i < houses.length; i++){
+        if(houses[i].name == name){
+            return houses[i].motto;
+        }
+    }
+}
+
+function mottoB(name) {
+    return houses.find(x => x.name == name).motto;
+}
