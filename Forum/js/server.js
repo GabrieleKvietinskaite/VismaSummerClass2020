0d59c2690e25970459d3032b875a239512951684
gabrielekvietinskaite <gabriele.kvietinskaite@gmail.com>
2020-08-05 21:42:47 +0300

    Add create, update, delete posts

--- Forum/js/server.js
+++ Forum/js/server.js
@@ -4,8 +4,16 @@ const server = express();
 const path = require('path');
 
 server.use(express.static(path.join(__dirname, '../')));
-
 server.use('/api', jsonServer.router('../database/db.json'));
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
+
 server.listen(3000, () => {
   console.log('Server is running')
 });
