5e2bdfc5df8550e2d1ebcecf1ea5d4f313aab726
gabrielekvietinskaite <gabriele.kvietinskaite@gmail.com>
2020-08-12 17:03:28 +0300

    Add sidebars components

--- Forum on Angular/forum/src/app/app.module.ts
+++ Forum on Angular/forum/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
+import { LeftSideBarComponent } from './Components/left-sidebar/left-sidebar.component';
+import { RightSidebarComponent } from './Components/right-sidebar/right-sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LeftSideBarComponent,
+    RightSidebarComponent
   ],
   imports: [
     BrowserModule,
